6a48feb5efa4168945cf4b5cf5cb327af26ba7e8
ChenYingYu <alan831007@gmail.com>
2020-12-06 10:59:48 +0800

    feat: Setup app navigation
    UITabBarController & UINavigationController

---
+++ GHFollowers/FavoritesListVC.swift
@@ -0,0 +1,16 @@
+//
+//  FavoritesListVC.swift
+//  GHFollowers
+//
+//  Created by ChenAlan on 2020/12/6.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+}
---
+++ GHFollowers/SearchVC.swift
@@ -0,0 +1,16 @@
+//
+//  SearchVC.swift
+//  GHFollowers
+//
+//  Created by ChenAlan on 2020/12/6.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+}
