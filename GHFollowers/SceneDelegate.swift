a8744395cadacf4e178c9100ae7de3074ea1e5a0
ChenYingYu <alan831007@gmail.com>
2020-12-08 19:44:14 +0800

    feat: Set navigation bar tint color to .systemGreen

--- GHFollowers/SceneDelegate.swift
+++ GHFollowers/SceneDelegate.swift
@@ -18,6 +18,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         window?.windowScene = windowScene
         window?.rootViewController = createTabbar()
         window?.makeKeyAndVisible()
+
+        configureNavigationBar()
     }
 
     // MARK: - Helper
@@ -43,6 +45,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         tabbar.viewControllers = [createSearchNC(), createFavoritesNC()]
 
         return tabbar
+    }
+
+    func configureNavigationBar() {
+        UINavigationBar.appearance().tintColor = .systemGreen
     }
 }
 
