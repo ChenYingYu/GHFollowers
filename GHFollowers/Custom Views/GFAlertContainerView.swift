349277654772f812a1a439eded8d22563ee6685b
ChenYingYu <alan831007@gmail.com>
2020-12-09 19:49:31 +0800

    refactor: Create GFAlertContainerView

---
+++ GHFollowers/Custom Views/GFAlertContainerView.swift
@@ -0,0 +1,28 @@
+//
+//  GFAlertContainerView.swift
+//  GHFollowers
+//
+//  Created by ChenAlan on 2020/12/9.
+//
+
+import UIKit
+
+class GFAlertContainerView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func configure() {
+        backgroundColor = .systemBackground
+        layer.cornerRadius = 16
+        layer.borderWidth = 2
+        layer.borderColor = UIColor.white.cgColor
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
