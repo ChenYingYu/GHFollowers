cfb73a824cb342827e71f5e688c646bd493c3c00
ChenYingYu <alan831007@gmail.com>
2020-12-16 07:57:31 +0800

    feat: Implement date label in UserInfoVC

---
+++ GHFollowers/Extensions/Date+Ext.swift
@@ -0,0 +1,16 @@
+//
+//  Date+Ext.swift
+//  GHFollowers
+//
+//  Created by ChenAlan on 2020/12/16.
+//
+
+import Foundation
+
+extension Date {
+    func convertToMonthYearFormat() -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MMM yyyy"
+        return dateFormatter.string(from: self)
+    }
+}
